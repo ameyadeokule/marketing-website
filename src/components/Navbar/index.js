8a8cb5c3b3f2e6e2ec8cb5c6e38c7438adcae05d
ameyadeokule <deokuleameya@gmail.com>
2021-03-10 17:14:04 +0530

    Updated the Navbar to adjust for Hero Section

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -17,7 +17,7 @@ const Navbar = ({ toggle }) => {
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo> Algotrade</NavLogo>
+          <NavLogo to='/'> Algotrade</NavLogo>
           <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
